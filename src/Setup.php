6dc1635369d9710e5ad8f12073dc4ce478bf2ece
Dion Purushotham <mail@dion.codes>
2021-03-13 14:08:19 +0100

    fix(setup): exception thrown when successful
    Signed-off-by: Dion Purushotham <mail@dion.codes>

--- src/Setup.php
+++ src/Setup.php
@@ -9,14 +9,14 @@ use TinyApps\DbUpdater\Exceptions\TableSetupException;
 class Setup {
 	public static function createConfigTable(PDO $conn) {
 		try {
-			if (!$conn->exec('
+			if ($conn->exec('
 			CREATE TABLE `database_updates` (
 				`id` bigint(20) unsigned,
 				`update_id` varchar(255) NOT NULL,
 				`execution_date` datetime NOT NULL DEFAULT NOW(),
 				PRIMARY KEY (id),
 				UNIQUE KEY `update_idx` (`update_id`) USING BTREE
-			)')) {
+			)') === false) {
 				throw new TableSetupException('Unable to create the database updates table. Is it already existing? If not, please try to create it manually.');
 			}
 		} catch (PDOException $e) {
